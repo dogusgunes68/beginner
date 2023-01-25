42a5b9e0e8e17f5f15d1d81749dea963f16d64d3
Dogus Gunes <dogusgunes68@gmail.com>
2023-01-25 21:56:35 +0300

    components

---
+++ src/components/ClassComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ClassComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is class component</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,9 @@
+function FunctionalComponent() {
+  return (
+    <div>
+      <h1>This is functional component</h1>
+    </div>
+  );
+}
+
+export default FunctionalComponent;
