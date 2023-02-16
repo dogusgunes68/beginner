2971e8fe9f7f5518127954bab66ea5fd7ae47efd
Dogus Gunes <dogusgunes68@gmail.com>
2023-02-16 23:38:56 +0300

    defined fragment

---
+++ src/components/fragments/FragmentComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FragmentComponent = () => {
+  //Define a fragment
+  return (
+    <>
+      <h1>Heading</h1>
+      <h2>Subheading</h2>
+    </>
+  );
+};
+
+export default FragmentComponent;
