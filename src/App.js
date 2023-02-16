d620a1c3321795166b285094cc926d838643fb07
Dogus Gunes <dogusgunes68@gmail.com>
2023-02-16 23:25:22 +0300

    beginner completed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@
 //import ClassComponent from "./components/ClassComponent";
 //import FunctionEvent from "./components/FunctionEvent";
 //import ClassEvent from "./components/ClassEvent";
-import RenderListUsingKey from "./components/RenderListUsingKey";
-import HandlingUserInput from "./components/HandlingUserInput";
+//import RenderListUsingKey from "./components/RenderListUsingKey";
+//import HandlingUserInput from "./components/HandlingUserInput";
+//import InlineComponent from "./components/todo/css/InlineComponent";
+import ModuleComponent from "./components/todo/css/ModuleComponent";
+//import StylesheetComponent from "./components/todo/css/StylesheetComponent";
+import Todo from "./components/todo/Todo";
 
 function App() {
   return (
     <div className="App">
-      <RenderListUsingKey></RenderListUsingKey>
-      <HandlingUserInput></HandlingUserInput>
+      <ModuleComponent></ModuleComponent>
+      <Todo></Todo>
     </div>
   );
 }
