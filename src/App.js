da41faf83fe0f048f81d62dca3db157f82cc3094
Dogus Gunes <dogusgunes68@gmail.com>
2023-02-16 23:51:27 +0300

    fragmet usage

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@
 //import RenderListUsingKey from "./components/RenderListUsingKey";
 //import HandlingUserInput from "./components/HandlingUserInput";
 //import InlineComponent from "./components/todo/css/InlineComponent";
-import ModuleComponent from "./components/todo/css/ModuleComponent";
+import Table from "./components/fragments/Table";
+//import ModuleComponent from "./components/todo/css/ModuleComponent";
 //import StylesheetComponent from "./components/todo/css/StylesheetComponent";
-import Todo from "./components/todo/Todo";
+//import Todo from "./components/todo/Todo";
 
 function App() {
   return (
     <div className="App">
-      <ModuleComponent></ModuleComponent>
-      <Todo></Todo>
+      <Table />
     </div>
   );
 }
---
+++ src/components/fragments/Column.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Column() {
+  return (
+    <React.Fragment>
+      <td>hello</td>
+      <td>There</td>
+    </React.Fragment>
+  );
+}
